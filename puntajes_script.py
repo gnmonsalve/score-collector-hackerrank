d3b4b1bfffc0d8923e0a77dbc48734cdca25a6bd
Gerinelda <gnmonsalve@uc.cl>
2018-09-02 16:05:24 -0300

    Fecha actualizada

--- puntajes_script.py
+++ puntajes_script.py
@@ -32,73 +32,73 @@ LABS = {
         'id': 'lab1',
         'link': 'iic1103-2018-2-lab1',
         'start': datetime(2018, 3, 17, 0),
-        'end': datetime(2018, 12, 27, 15, 30)
+        'end': datetime(2018, 8, 30, 15, 30)
         },
     'lab2': {
         'id': 'lab2',
         'link': 'iic1103-2018-2-lab2',
         'start': datetime(2018, 3, 20, 0),
-        'end': datetime(2018, 12, 27, 15, 30)
+        'end': datetime(2018, 9, 6, 15, 30)
         },
     'lab3': {
         'id': 'lab3',
         'link': 'iic1103-2018-2-lab3',
         'start': datetime(2018, 3, 20, 0),
-        'end': datetime(2018, 12, 27, 15, 30)
+        'end': datetime(2018, 9, 13, 15, 30)
         },
     'lab4': {
         'id': 'lab4',
         'link': 'iic1103-2018-2-lab4',
         'start': datetime(2018, 3, 20, 0),
-        'end': datetime(2018, 12, 27, 15, 30)
+        'end': datetime(2018, 9, 20, 15, 30)
         },
     'lab5': {
         'id': 'lab5',
         'link': 'iic1103-2018-2-lab5',
         'start': datetime(2018, 3, 23, 0),
-        'end': datetime(2018, 12, 27, 15, 30)
+        'end': datetime(2018, 10, 4, 15, 30)
         },
     'lab6': {
         'id': 'lab6',
         'link': 'iic1103-2018-2-lab6',
         'start': datetime(2018, 3, 23, 0),
-        'end': datetime(2018, 12, 27, 15, 30)
+        'end': datetime(2018, 10, 11, 15, 30)
         },
     'lab7': {
         'id': 'lab7',
         'link': 'iic1103-2018-2-lab7',
         'start': datetime(2018, 4, 23, 0),
-        'end': datetime(2018, 12, 27, 15, 30)
+        'end': datetime(2018, 10, 18, 15, 30)
         },
     'lab8': {
         'id': 'lab8',
         'link': 'iic1103-2018-2-lab8',
         'start': datetime(2018, 3, 23, 0),
-        'end': datetime(2018, 12, 27, 15, 30)
+        'end': datetime(2018, 10, 25, 15, 30)
         },
     'lab9': {
         'id': 'lab9',
         'link': 'iic1103-2018-2-lab9',
         'start': datetime(2018, 3, 23, 0),
-        'end': datetime(2018, 12, 27, 15, 30)
+        'end': datetime(2018, 11, 1, 15, 30)
         },
     'lab10': {
         'id': 'lab10',
         'link': 'iic1103-2018-2-lab10',
         'start': datetime(2018, 3, 23, 0),
-        'end': datetime(2018, 12, 27, 15, 30)
+        'end': datetime(2018, 11, 8, 15, 30)
         },
     'lab11': {
         'id': 'lab11',
         'link': 'iic1103-2018-2-lab11',
         'start' : datetime(2018, 3, 23, 0),
-        'end': datetime(2018, 12, 27, 15, 30)
+        'end': datetime(2018, 11, 15, 15, 30)
         },
     'lab12': {
         'id': 'lab12',
         'link': 'iic1103-2018-2-lab12',
         'start' : datetime(2018, 3, 23, 0),
-        'end': datetime(2018, 12, 27, 15, 30)
+        'end': datetime(2018, 11, 22, 15, 30)
     },
 }
 
@@ -167,9 +167,8 @@ def actualizar(service):
 
         for row in file:
             puntajes = {}
-
             for i in range(0,11):
-                inicial, actual = int(row[2 + 2*i]), int(row[2 + 2*i + 1])
+                inicial, actual = int(row[2 + 2*i-1]), int(row[2 + 2*i])
 
                 puntajes[lab_keys[i]] = [inicial, actual]
             alumnos[row[0]] = puntajes
