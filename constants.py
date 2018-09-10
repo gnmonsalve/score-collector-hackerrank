02b2717b9579f4f77fa2075498dff1717d2d1b78
Gerinelda <gnmonsalve@uc.cl>
2018-09-10 11:02:16 -0300

    Archivo constantes agregado

---
+++ constants.py
@@ -0,0 +1,78 @@
+from datetime import datetime
+
+SPREADSHEET_ID = '12VA_7AUpKuUSXFQzmlWmNl722iVwgp6bAPKgO2g7r9I'
+
+LABS = {
+    'lab1': {
+        'id': 'lab1',
+        'link': 'iic1103-2018-2-lab1',
+        'start': datetime(2018, 3, 17, 0),
+        'end': datetime(2018, 8, 30, 15, 30)
+        },
+    'lab2': {
+        'id': 'lab2',
+        'link': 'iic1103-2018-2-lab2',
+        'start': datetime(2018, 3, 20, 0),
+        'end': datetime(2018, 9, 6, 15, 30)
+        },
+    'lab3': {
+        'id': 'lab3',
+        'link': 'iic1103-2018-2-lab3',
+        'start': datetime(2018, 3, 20, 0),
+        'end': datetime(2018, 9, 13, 15, 30)
+        },
+    'lab4': {
+        'id': 'lab4',
+        'link': 'iic1103-2018-2-lab4',
+        'start': datetime(2018, 3, 20, 0),
+        'end': datetime(2018, 9, 20, 15, 30)
+        },
+    'lab5': {
+        'id': 'lab5',
+        'link': 'iic1103-2018-2-lab5',
+        'start': datetime(2018, 3, 23, 0),
+        'end': datetime(2018, 10, 4, 15, 30)
+        },
+    'lab6': {
+        'id': 'lab6',
+        'link': 'iic1103-2018-2-lab6',
+        'start': datetime(2018, 3, 23, 0),
+        'end': datetime(2018, 10, 11, 15, 30)
+        },
+    'lab7': {
+        'id': 'lab7',
+        'link': 'iic1103-2018-2-lab7',
+        'start': datetime(2018, 4, 23, 0),
+        'end': datetime(2018, 10, 18, 15, 30)
+        },
+    'lab8': {
+        'id': 'lab8',
+        'link': 'iic1103-2018-2-lab8',
+        'start': datetime(2018, 3, 23, 0),
+        'end': datetime(2018, 10, 25, 15, 30)
+        },
+    'lab9': {
+        'id': 'lab9',
+        'link': 'iic1103-2018-2-lab9',
+        'start': datetime(2018, 3, 23, 0),
+        'end': datetime(2018, 11, 1, 15, 30)
+        },
+    'lab10': {
+        'id': 'lab10',
+        'link': 'iic1103-2018-2-lab10',
+        'start': datetime(2018, 3, 23, 0),
+        'end': datetime(2018, 11, 8, 15, 30)
+        },
+    'lab11': {
+        'id': 'lab11',
+        'link': 'iic1103-2018-2-lab11',
+        'start' : datetime(2018, 3, 23, 0),
+        'end': datetime(2018, 11, 15, 15, 30)
+        },
+    'lab12': {
+        'id': 'lab12',
+        'link': 'iic1103-2018-2-lab12',
+        'start' : datetime(2018, 3, 23, 0),
+        'end': datetime(2018, 11, 22, 15, 30)
+    },
+}
--- puntajes_script.py
+++ puntajes_script.py
@@ -1,5 +1,4 @@
 # Spreadsheet
-
 from __future__ import print_function
 from apiclient.discovery import build
 from httplib2 import Http
@@ -23,84 +22,9 @@ import json
 import sys
 import csv
 
-SPREADSHEET_ID = '1jkUBemOvFFGbaUna2VLDWjw-xKb0iI6YpVW5u_n2x-g'
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
-service = None
+from constants import LABS, SPREADSHEET_ID
 
-LABS = {
-    'lab1': {
-        'id': 'lab1',
-        'link': 'iic1103-2018-2-lab1',
-        'start': datetime(2018, 3, 17, 0),
-        'end': datetime(2018, 8, 30, 15, 30)
-        },
-    'lab2': {
-        'id': 'lab2',
-        'link': 'iic1103-2018-2-lab2',
-        'start': datetime(2018, 3, 20, 0),
-        'end': datetime(2018, 9, 6, 15, 30)
-        },
-    'lab3': {
-        'id': 'lab3',
-        'link': 'iic1103-2018-2-lab3',
-        'start': datetime(2018, 3, 20, 0),
-        'end': datetime(2018, 9, 13, 15, 30)
-        },
-    'lab4': {
-        'id': 'lab4',
-        'link': 'iic1103-2018-2-lab4',
-        'start': datetime(2018, 3, 20, 0),
-        'end': datetime(2018, 9, 20, 15, 30)
-        },
-    'lab5': {
-        'id': 'lab5',
-        'link': 'iic1103-2018-2-lab5',
-        'start': datetime(2018, 3, 23, 0),
-        'end': datetime(2018, 10, 4, 15, 30)
-        },
-    'lab6': {
-        'id': 'lab6',
-        'link': 'iic1103-2018-2-lab6',
-        'start': datetime(2018, 3, 23, 0),
-        'end': datetime(2018, 10, 11, 15, 30)
-        },
-    'lab7': {
-        'id': 'lab7',
-        'link': 'iic1103-2018-2-lab7',
-        'start': datetime(2018, 4, 23, 0),
-        'end': datetime(2018, 10, 18, 15, 30)
-        },
-    'lab8': {
-        'id': 'lab8',
-        'link': 'iic1103-2018-2-lab8',
-        'start': datetime(2018, 3, 23, 0),
-        'end': datetime(2018, 10, 25, 15, 30)
-        },
-    'lab9': {
-        'id': 'lab9',
-        'link': 'iic1103-2018-2-lab9',
-        'start': datetime(2018, 3, 23, 0),
-        'end': datetime(2018, 11, 1, 15, 30)
-        },
-    'lab10': {
-        'id': 'lab10',
-        'link': 'iic1103-2018-2-lab10',
-        'start': datetime(2018, 3, 23, 0),
-        'end': datetime(2018, 11, 8, 15, 30)
-        },
-    'lab11': {
-        'id': 'lab11',
-        'link': 'iic1103-2018-2-lab11',
-        'start' : datetime(2018, 3, 23, 0),
-        'end': datetime(2018, 11, 15, 15, 30)
-        },
-    'lab12': {
-        'id': 'lab12',
-        'link': 'iic1103-2018-2-lab12',
-        'start' : datetime(2018, 3, 23, 0),
-        'end': datetime(2018, 11, 22, 15, 30)
-    },
-}
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
 
 def on_time(start, end, actual):
     return start <= actual <= end
@@ -227,7 +151,7 @@ if __name__ == '__main__':
     store = file.Storage('token.json')
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
         creds = tools.run_flow(flow, store)
     service = build('sheets', 'v4', http=creds.authorize(Http()))
 
